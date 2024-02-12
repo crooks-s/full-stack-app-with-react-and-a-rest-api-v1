6665e83b9d342c686e5e96d6e7be5df07ffe074a
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-11 19:51:13 -0800

    fix layout and add classes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,26 @@
-const Header = () => {
+import { Link } from 'react-router-dom';
 
+const Header = () => {
+  return (
+    <header>
+      <div className='wrap header--flex'>
+        <div className='header--logo'>
+          <Link to='/'><h1 className='header--logo'>Courses</h1></Link>
+        </div>
+        <nav>
+          {/* add ternary logic to see if user logged in 
+              if logged in -- render sign out and a message(?) 
+              else render sign up and sign in 
+              NOTE: css class will be different also */}
+          <ul className='header--signedout'>
+            {/* Change href's */}
+            <li><a href='/'>Sign Up</a></li> 
+            <li><a href='/'>Sign in</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
