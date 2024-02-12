1248eba9afaa9bd72854cdf238831d0d4a56fcd1
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-11 20:05:29 -0800

    disable Home component for project

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import CourseDetail from './components/CourseDetail';
 import Courses from './components/Courses';
 import Header from './components/Header';
-import Home from './components/Home';
+// import Home from './components/Home';
 import NotFound from './components/NotFound';
 import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
@@ -16,8 +16,7 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} /> {/* will be able to CREATE and READ at this route */}
+        <Route path="/" element={<Courses />} /> {/* will be able to CREATE and READ at this route */}
         <Route path="/courses/:id" element={<CourseDetail />} /> {/* will be able to POST and DELETE at this route */}
         <Route path="/signin" element={<UserSignIn />} />
         <Route path="/signup" element={<UserSignUp />} />
