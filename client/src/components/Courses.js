dbab035bd7a2ee3bfdb7a06f95e30d82b20ebbf0
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-11 19:54:09 -0800

    add instructions comment

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
+/*
+Render COURSES screen -- 
+  1. get list of courses from API's '/api/courses' route then render the list
+  2. each course needs a LINK to its 'Course Detail' screen
+  3. render a link to 'Create Course' screen
+*/
+
 const Courses = () => {
 
 }
