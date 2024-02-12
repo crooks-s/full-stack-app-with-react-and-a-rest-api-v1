5318480e66ac585c71b299393948637f1f97b288
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-11 19:56:36 -0800

    add instructions comment

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
+/*
+Renders the "Course Detail" screen -- 
+  1. get course details from API's "/api/courses/:id" route and render the course
+  2. render DELETE COURSE button -- send delete request to API's "/api/courses/:id"
+  3. render UPDATE COURSE button -- navigate to "UPDATE COURSE" screen
+*/
+
 const CourseDetail = () => {
 
 }
