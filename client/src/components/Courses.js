a56dd9a8d569866026ff7f64ed69a4e54cb735e4
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-12 16:13:26 -0800

    add fetch to display courses

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,14 +1,54 @@
-import { useState } from "react";
-
-/*
-Render COURSES screen -- 
-  1. get list of courses from API's '/api/courses' route then render the list
-  2. each course needs a LINK to its 'Course Detail' screen
-  3. render a link to 'Create Course' screen
-*/
+import { useState, useEffect } from "react";
 
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
 
+  useEffect(() => {
+    const fetchOptions = {
+      method: 'GET',
+      headers: {}
+    }
+    
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/courses', fetchOptions);
+        if (response.status === 200) {
+          const data = await response.json();
+          setCourses(data);
+        }
+      } catch (err) {
+        console.log('Error: ', err.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="wrap main--grid">
+      {courses.map(course => (
+        <a className='course--module course--link' key={course.id} href={`/courses/${course.id}`}>
+          <h2 className="course--label">Course</h2>
+          <h3 className="course--title">{course.title}</h3>
+        </a>
+      ))}
+      <a className="course--module course--add--module" href="/courses/create">
+        <span className="course--add--title">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 13 13"
+            className="add"
+          >
+            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6"></polygon>
+          </svg>
+          New Course
+        </span>
+      </a>
+    </div>
+  );
 }
 
 export default Courses;
