ed4b901d0b1481e120ac24c39cd7248b39e971a0
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-12 17:28:40 -0800

    change nav if user is signed in or signed out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,31 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import UserContext from '../context/UserContext';
+
 const Header = () => {
+  const { authUser } = useContext(UserContext);
+
   return (
     <header>
       <div className='wrap header--flex'>
-        <div className='header--logo'>
-          <Link to='/'><h1 className='header--logo'>Courses</h1></Link>
-        </div>
+        <h1 className='header--logo'><a href='/'>Courses</a></h1>
         <nav>
-          {/* add ternary logic to see if user logged in 
-              if logged in -- render sign out and a message(?) 
-              else render sign up and sign in 
-              NOTE: css class will be different also */}
-          <ul className='header--signedout'>
-            {/* Change href's */}
-            <li><a href='/'>Sign Up</a></li> 
-            <li><a href='/signin'>Sign in</a></li>
-          </ul>
+          {authUser ?
+            <>
+              {/* class should be header--signedin but has a li marker */}
+              <ul className='header--signedout'>
+                <li><a href='/signout'>Sign Out</a></li>
+              </ul>
+            </>
+            :
+            <>
+              <ul className='header--signedout'>
+                <li><a href='/signup'>Sign Up</a></li>
+                <li><a href='/signin'>Sign in</a></li>
+              </ul>
+            </>
+          }
         </nav>
       </div>
     </header>
