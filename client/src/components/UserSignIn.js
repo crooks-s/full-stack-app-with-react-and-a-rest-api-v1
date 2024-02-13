55b113061bb78b6d93619aca3f3c15e6d89eae21
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-12 16:43:56 -0800

    clean up comments

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -3,13 +3,6 @@ import { useNavigate } from "react-router-dom";
 
 import UserContext from "../context/UserContext";
 
-/*
-Renders the "Sign in" screen -- 
-  1. render a FORM that allows user to sign IN using existing acct. info
-  2. render sign IN button -- signs in user
-  3. render cancel button that returns user to default route -- list of courses
-*/
-
 const UserSignIn = () => {
   const navigate = useNavigate();
   const { actions } = useContext(UserContext);
