3fd74fdb57e1b668d9f8d53074a39a4a30938963
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-12 17:37:10 -0800

    add signout function

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,7 +1,15 @@
 // sign out authenticated user and redirect to default route
+import { useContext, useEffect } from "react";
+import { Navigate } from 'react-router-dom';
+
+import UserContext from "../context/UserContext";
 
 const UserSignOut = () => {
+  const { actions } = useContext(UserContext);
 
+  useEffect(() => actions.signOut());
+
+  return <Navigate to="/" replace />;
 }
 
 export default UserSignOut;
