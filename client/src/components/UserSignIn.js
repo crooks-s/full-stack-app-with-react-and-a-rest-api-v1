42b949b3cb7d7fba576863638173dcf5ca344d7d
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-12 16:43:18 -0800

    add signin function using existing info on db

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useContext, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import UserContext from "../context/UserContext";
 
 /*
 Renders the "Sign in" screen -- 
@@ -8,7 +11,63 @@ Renders the "Sign in" screen --
 */
 
 const UserSignIn = () => {
+  const navigate = useNavigate();
+  const { actions } = useContext(UserContext);
 
+  const emailAddress = useRef(null);
+  const password = useRef(null);
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+
+    const credentials = {
+      emailAddress: emailAddress.current.value,
+      password: password.current.value
+    };
+
+    try { 
+      const user = await actions.signIn(credentials);
+      if (user) {
+        console.log("You're logged in.");
+        navigate('/'); // will need to show user is logged in
+      } else {
+        console.log('sign in unsuccessful');
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    navigate('/');
+  }
+
+  return (
+    <div className="form--centered">
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="emailAddress">Email Address</label>
+        <input 
+          id="emailAddress"
+          name="emailAddress"
+          type="email"
+          ref={emailAddress}
+          placeholder="email@example.com"
+        />
+        <label htmlFor="password">Password</label>
+        <input 
+        id="password"
+        name="password"
+        type="password"
+        ref={password}
+        placeholder="8-20 characters"
+        />
+        <button className='button' type="submit">Sign In</button>
+        <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+      </form>
+    </div>
+  );
 }
 
 export default UserSignIn;
--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -5,6 +5,49 @@ const UserContext = createContext(null);
 export const UserProvider = (props) => {
   const [authUser, setAuthUser] = useState(null);
 
+  const signIn = async (credentials) => {
+
+    const fetchOptions = {
+      method: 'GET',
+      headers: {}
+    }
+
+    const encodedCredentials = btoa(`${credentials.emailAddress}:${credentials.password}`);
+    fetchOptions.headers.Authorization = `Basic ${encodedCredentials}`;
+
+    try {
+      const response = await fetch('http://localhost:5000/api/users', fetchOptions);
+      if (response.status === 200) {
+        const user = await response.json();
+        setAuthUser(user);
+        return user;
+      } else if (response.status === 401) {
+        console.log('Unauthorized: Invalid credentials');
+        return null;
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.log('Error: ', error.message);
+    }
+
+  }
+
+  const signOut = () => {
+    setAuthUser(null);
+  }
+
+  return (
+    <UserContext.Provider value={{
+      authUser,
+      actions: {
+        signIn,
+        signOut
+      }
+    }}>
+      {props.children}
+    </UserContext.Provider>
+  );
 }
 
 export default UserContext;
