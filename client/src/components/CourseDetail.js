2bbcb84ecc87e284d1e053973f1ecd032745d0de
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-13 20:31:49 -0800

    fetch and render course details

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 /*
 Renders the "Course Detail" screen -- 
@@ -8,6 +9,60 @@ Renders the "Course Detail" screen --
 */
 
 const CourseDetail = () => {
+  const [course, setCourse] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchOptions = {
+      method: 'GET',
+      headers: {}
+    }
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/courses/${id}`, fetchOptions);
+
+        if (response.status === 200) {
+          const data = await response.json();
+          setCourse(data);
+        }
+
+      } catch (error) {
+        console.log('Error: ', error.message);
+      }
+
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="wrap">
+      <h2>Course Detail</h2>
+      <form>
+        <div className="main--flex">
+          <div>
+            <h3 className="course--detail--title">Course</h3>
+            <h4 class="course--name">{course.title}</h4>
+            <p>By {course.User && `${course.User.firstName} ${course.User.lastName}`}</p>
+            <p>{course.description && course.description.split('\n\n').map((paragraph, index) => 
+            <p key={index}>{paragraph}</p> )}</p> 
+          </div>
+          <div>
+            <h3 className="course--detail--title">Estimated Time</h3>
+            <p>{course.estimatedTime}</p>
+            <h3 className="course--detail--title">Materials Needed</h3>
+            <ul className="course--detail--list">
+              {course.materialsNeeded && course.materialsNeeded.substring(1).split('*').map((material, index) => (
+                <li key={index}>{material}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+
 
 }
 
