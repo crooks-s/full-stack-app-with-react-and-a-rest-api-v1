3b765aab7ebe2a77647cc65221a4cbf493ef2dde
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-15 15:44:30 -0800

    cleanup, add comments for clarity

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,10 +1,13 @@
+// Modules
 import { useContext } from "react"
 import { Navigate, Outlet } from 'react-router-dom';
+
+// Context
 import UserContext from "../context/UserContext";
 
 const PrivateRoute = () => {
   const { authUser } = useContext(UserContext);
-
+  
   if (authUser) {
     return <Outlet />
   } else {
