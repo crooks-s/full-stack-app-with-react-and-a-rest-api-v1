41397fc022d10656d56389a8b156df4f90323f7f
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-15 17:46:03 -0800

    add update course functionailty, cleanup

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,22 +1,18 @@
 // Modules
 import { useState, useRef, useEffect, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+// Context
 import UserContext from "../context/UserContext";
+// Component
 import ErrorsDisplay from "./ErrorsDisplay";
 
-/*
-Renders the "UPDATE COURSE" screen -- 
-  1. render a FORM that allows user to update one of their existing courses
-  2. render update course button - sends POST req to /api/courses/:id onClick
-  3. render cancel button that returns user to Course Detail screen
-*/
-
 const UpdateCourse = () => {
-  // Hooks
+  // React Hooks
   const [course, setCourse] = useState([]);
   const [errors, setErrors] = useState([]);
   const navigate = useNavigate();
   const { authUser } = useContext(UserContext);
+  const { id } = useParams();
 
   // React Refs
   const title = useRef(null);
@@ -24,16 +20,12 @@ const UpdateCourse = () => {
   const estimatedTime = useRef(null);
   const materialsNeeded = useRef(null);
 
-
-  // will use param to redirect to its course detail page
-  const { id } = useParams();
-  
-  // Retrieve course details to update for a single course at id
+  // Retrieve course details to update a single course
   useEffect(() => {
     const fetchOptions = {
       method: 'GET',
       headers: {}
-    }
+    };
     const fetchData = async () => {
       try {
         const response = await fetch(`http://localhost:5000/api/courses/${id}`, fetchOptions);
@@ -44,57 +36,57 @@ const UpdateCourse = () => {
       } catch (error) {
         console.log('Error: ', error.message);
       }
-    }
+    };
     fetchData();
   }, [id]);
 
-    // Update Course
-    const handleSubmit = async (event) => {
-      event.preventDefault(); 
-      const updatedCourse = {
-        title: title.current.value,
-        description: description.current.value,
-        estimatedTime: estimatedTime.current.value,
-        materialsNeeded: materialsNeeded.current.value,
-        userId: authUser.user.id
-      };
-      const encodedCredentials = btoa(
-        `${authUser.user.emailAddress}:${authUser.user.password}`
-      );
-      const fetchOptions = {
-        method: 'PUT',
-        body: JSON.stringify(updatedCourse),
-        headers: {
+  // Update Course
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const updatedCourse = {
+      title: title.current.value,
+      description: description.current.value,
+      estimatedTime: estimatedTime.current.value,
+      materialsNeeded: materialsNeeded.current.value,
+      userId: authUser.user.id
+    };
+    const encodedCredentials = btoa(
+      `${authUser.user.emailAddress}:${authUser.user.password}`
+    );
+    const fetchOptions = {
+      method: 'PUT',
+      body: JSON.stringify(updatedCourse),
+      headers: {
         "Content-Type": "application/json; charset=utf-8",
         Authorization: `Basic ${encodedCredentials}`
-        }
-      };
-      try {
-        const response = await fetch(
-          `http://localhost:5000/api/courses/${id}`,
-          fetchOptions
-        );
-        if (response.status === 204) {
-          navigate(`/courses/${id}`);
-        } else if (response.status === 400) {
-          const data = await response.json();
-          setErrors(data.errors);
-        } else if (response.status === 401) {
-          const data = await response.json();
-          setErrors([data.message]);
-        } else {
-          throw new Error();
-        }
-      } catch (error) {
-        console.log('Error: ', error.message);
       }
     };
-  
-    // cancel button
-    const handleCancel = (e) => {
-      e.preventDefault();
-      navigate('/');
-    }
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/courses/${id}`,
+        fetchOptions
+      );
+      if (response.status === 204) {
+        navigate(`/courses/${id}`);
+      } else if (response.status === 400) {
+        const data = await response.json();
+        setErrors(data.errors);
+      } else if (response.status === 401) {
+        const data = await response.json();
+        setErrors([data.message]);
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.log('Error: ', error.message);
+    };
+  };
+
+  // For Cancel button
+  const handleCancel = (e) => {
+    e.preventDefault();
+    navigate('/');
+  };
 
   return (
     <>
@@ -104,7 +96,7 @@ const UpdateCourse = () => {
         <form onSubmit={handleSubmit}>
           <div className="main--flex">
             <div>
-              <label for="courseTitle">Course Title</label>
+              <label htmlFor="courseTitle">Course Title</label>
               <input
                 id="courseTitle"
                 name="courseTitle"
@@ -112,8 +104,10 @@ const UpdateCourse = () => {
                 ref={title}
                 defaultValue={course.title}
               />
-              <p>By {course.User && `${course.User.firstName} ${course.User.lastName}`}</p>
-              <label for="courseDescription">Course Description</label>
+              <p>
+                By {course.User && `${course.User.firstName} ${course.User.lastName}`}
+              </p>
+              <label htmlFor="courseDescription">Course Description</label>
               <textarea
                 id="courseDescription"
                 name="courseDescription"
@@ -123,7 +117,7 @@ const UpdateCourse = () => {
               />
             </div>
             <div>
-              <label for="estimatedTime">Estimated Time</label>
+              <label htmlFor="estimatedTime">Estimated Time</label>
               <input
                 id="estimatedTime"
                 name="estimatedTime"
@@ -131,7 +125,7 @@ const UpdateCourse = () => {
                 ref={estimatedTime}
                 defaultValue={course.estimatedTime}
               />
-              <label for="materialsNeeded">Materials Needed</label>
+              <label htmlFor="materialsNeeded">Materials Needed</label>
               <textarea
                 id="materialsNeeded"
                 name="materialsNeeded"
