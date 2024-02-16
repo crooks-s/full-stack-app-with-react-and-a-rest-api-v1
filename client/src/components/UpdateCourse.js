dc01142edd2df1405230516cdf9cd82d3d6f1763
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-15 17:25:59 -0800

    add update course functionality, cleanup, add errors display

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,5 +1,8 @@
-import { useState, useRef, useEffect } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+// Modules
+import { useState, useRef, useEffect, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import UserContext from "../context/UserContext";
+import ErrorsDisplay from "./ErrorsDisplay";
 
 /*
 Renders the "UPDATE COURSE" screen -- 
@@ -11,52 +14,94 @@ Renders the "UPDATE COURSE" screen --
 const UpdateCourse = () => {
   // Hooks
   const [course, setCourse] = useState([]);
-  const courseTitle = useRef();
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+  const { authUser } = useContext(UserContext);
+
+  // React Refs
+  const title = useRef(null);
+  const description = useRef(null);
   const estimatedTime = useRef(null);
-  const courseDescription = useRef(null);
   const materialsNeeded = useRef(null);
 
-  const navigate = useNavigate();
 
   // will use param to redirect to its course detail page
   const { id } = useParams();
-
-  // handleSubmit
-
-  // cancel button
-  const handleCancel = (e) => {
-    e.preventDefault();
-    navigate('/');
-  }
-
+  
+  // Retrieve course details to update for a single course at id
   useEffect(() => {
     const fetchOptions = {
       method: 'GET',
       headers: {}
     }
-
     const fetchData = async () => {
       try {
         const response = await fetch(`http://localhost:5000/api/courses/${id}`, fetchOptions);
-
         if (response.status === 200) {
           const data = await response.json();
           setCourse(data);
         }
-
       } catch (error) {
         console.log('Error: ', error.message);
       }
     }
-
     fetchData();
-  }, []);
+  }, [id]);
+
+    // Update Course
+    const handleSubmit = async (event) => {
+      event.preventDefault(); 
+      const updatedCourse = {
+        title: title.current.value,
+        description: description.current.value,
+        estimatedTime: estimatedTime.current.value,
+        materialsNeeded: materialsNeeded.current.value,
+        userId: authUser.user.id
+      };
+      const encodedCredentials = btoa(
+        `${authUser.user.emailAddress}:${authUser.user.password}`
+      );
+      const fetchOptions = {
+        method: 'PUT',
+        body: JSON.stringify(updatedCourse),
+        headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: `Basic ${encodedCredentials}`
+        }
+      };
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/courses/${id}`,
+          fetchOptions
+        );
+        if (response.status === 204) {
+          navigate(`/courses/${id}`);
+        } else if (response.status === 400) {
+          const data = await response.json();
+          setErrors(data.errors);
+        } else if (response.status === 401) {
+          const data = await response.json();
+          setErrors([data.message]);
+        } else {
+          throw new Error();
+        }
+      } catch (error) {
+        console.log('Error: ', error.message);
+      }
+    };
+  
+    // cancel button
+    const handleCancel = (e) => {
+      e.preventDefault();
+      navigate('/');
+    }
 
   return (
     <>
       <div className="wrap">
+        <ErrorsDisplay errors={errors.map(error => error.msg || error)} />
         <h2>Update Course</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="main--flex">
             <div>
               <label for="courseTitle">Course Title</label>
@@ -64,7 +109,7 @@ const UpdateCourse = () => {
                 id="courseTitle"
                 name="courseTitle"
                 type="text"
-                ref={courseTitle}
+                ref={title}
                 defaultValue={course.title}
               />
               <p>By {course.User && `${course.User.firstName} ${course.User.lastName}`}</p>
@@ -72,7 +117,7 @@ const UpdateCourse = () => {
               <textarea
                 id="courseDescription"
                 name="courseDescription"
-                ref={courseDescription}
+                ref={description}
                 defaultValue={course.description}
                 style={{ resize: 'none' }}
               />
@@ -90,14 +135,14 @@ const UpdateCourse = () => {
               <textarea
                 id="materialsNeeded"
                 name="materialsNeeded"
-                useRef={materialsNeeded}
+                ref={materialsNeeded}
                 defaultValue={course.materialsNeeded}
                 style={{ resize: 'none' }}
               />
             </div>
           </div>
           <button className="button" type="submit">Update Button</button>
-          <button class="button button-secondary" onClick={handleCancel}>Cancel</button>
+          <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
         </form>
       </div>
     </>
