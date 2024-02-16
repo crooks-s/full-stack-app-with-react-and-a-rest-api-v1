f4fa37af000cbb033964b00d33d02325d44277fa
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-15 18:43:07 -0800

    clean up

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,22 +1,23 @@
+// Modules
 import { createContext, useState } from "react";
 import Cookies from "js-cookie";
-
+// Initialize Context
 const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
+  // Retrieve cookie if exists
   const cookie = Cookies.get("authenticatedUser");
+  // React Hook to store if user is logged in or out
   const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
 
+  // SignIn Handler
   const signIn = async (credentials) => {
-
     const fetchOptions = {
       method: 'GET',
       headers: {}
-    }
-
+    };
     const encodedCredentials = btoa(`${credentials.emailAddress}:${credentials.password}`);
     fetchOptions.headers.Authorization = `Basic ${encodedCredentials}`;
-
     try {
       const response = await fetch('http://localhost:5000/api/users', fetchOptions);
       if (response.status === 200) {
@@ -27,7 +28,6 @@ export const UserProvider = (props) => {
         Cookies.set("authenticatedUser", JSON.stringify(user), {expires: 1});
         return user;
       } else if (response.status === 401) {
-        console.log('Unauthorized: Invalid credentials');
         return null;
       } else {
         throw new Error();
