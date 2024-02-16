1150bf0cdec2b3a60921f28c978b3a04e7124885
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-15 20:19:14 -0800

    add button for user to return to courses list

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => (
   <div className="bounds">
+    <div className="actions--bar">
+      <Link to='/' className="button button-secondary">
+        Return to List
+      </Link>
+    </div>
     <h1>404</h1>
     <p>Sorry! The page you were looking for cannot be found!</p>
   </div>
